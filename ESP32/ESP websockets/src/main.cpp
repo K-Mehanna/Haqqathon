60cc79661d91a87f5c7f20e8ae872daed65121ac
Idrees <33992400+idrees-mahmood@users.noreply.github.com>
2024-06-23 08:55:35 +0100

    LOTDaighaiodhgadhogihadgh

--- ESP32/ESP websockets/src/main.cpp
+++ ESP32/ESP websockets/src/main.cpp
@@ -2,58 +2,53 @@
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
 #include <ArduinoJson.h>
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+#include <SPI.h>
+#include <LoRa.h>
 
-#define ECG_PIN 23  // ECG sensor pin L0 +
-#define ECG_PIN2 22 // ECG sensor pin L0 -
+#define ECG_PIN 16  // ECG sensor pin L0 +
+#define ECG_PIN2 17 // ECG sensor pin L0 -
+
+#define LORA 14
 
 const char *ssid = "ESP32-Access-Point";
 const char *password = "123456789";
 
-// Create AsyncWebServer object on port 8080
 AsyncWebServer server(8080);
 AsyncWebSocket ws("/ws");
 
 JsonDocument doc;
+JsonArray ecgDataArray;
+JsonArray ecgTimeArray;
+
+JsonDocument doc2;
+
+MAX30105 particleSensor;
+
+#define MAX_BRIGHTNESS 255
+
+uint32_t irBuffer[100];
+uint32_t redBuffer[100];
+
+int32_t bufferLength;
+int32_t spo2;
+int8_t validSPO2;
+int32_t heartRate;
+int8_t validHeartRate;
 
 void setupJSON();
-float readECG();
-float readO2();
-float readheartRate();
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+void sendWsMessage();
+void readSensors();
 
-void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
-{
-  if (type == WS_EVT_CONNECT)
-  {
-    Serial.println("Websocket client connection received");
-  }
-  else if (type == WS_EVT_DISCONNECT)
-  {
-    Serial.println("Client disconnected");
-  }
-  else if (type == WS_EVT_DATA)
-  {
-    Serial.printf("Data received: %s\n", data);
-    // send message to client
-    client->text("Hello from ESP32 Server");
-  }
-}
-
-void sendWsMessage()
-{
-  // serialize the JSON document to a string
-  String json;
-  serializeJson(doc, json);
-  //Serial.println("Attemmpt" + json);
-  ws.textAll(json);
-  // ws.textAll("Hello from ESP32 Server");
-}
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   pinMode(ECG_PIN, INPUT);
   pinMode(ECG_PIN2, INPUT);
+  pinMode(LORA, OUTPUT);
 
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
@@ -63,88 +58,92 @@ void setup()
   ws.onEvent(onWebSocketEvent);
   server.addHandler(&ws);
 
-  // start server
   server.begin();
-  Serial.print("Setup complete");
+  Serial.println("Web server setup complete");
 
   setupJSON();
-}
 
-void loop()
-{
-  // Serial.println("Sending message");
-  readO2();
-  readheartRate();
-  readECG();
 
-  sendWsMessage();
-  delay(100);
-}
 
-void setupJSON()
-{
-  // doc["patientID"] = "123456";
-  JsonObject ecgdata = doc.createNestedObject("ecgdata");
-
-  JsonArray ecgDataArray = ecgdata.createNestedArray("data");
-  ecgDataArray.add("0.5");
-  ecgDataArray.add("0.7");
-  ecgDataArray.add("0.5");
-  ecgDataArray.add("0.7");
-  ecgDataArray.add("0.5");
-  ecgDataArray.add("0.7");
-  JsonArray ecgTimeArray = ecgdata.createNestedArray("time");
-  ecgTimeArray.add("0.5");
-  ecgTimeArray.add("0.7");
-  ecgTimeArray.add("0.5");
-  ecgTimeArray.add("0.7");
-  ecgTimeArray.add("0.5");
-  ecgTimeArray.add("0.7");
-
-  // doc["oxygendata"] = String(readO2());
-  doc["heartRate"] = String(readheartRate());
-
-  // serialize the JSON document to a string
-  String output;
-  serializeJson(doc, output);
-  Serial.println(output);
-}
-
-float readECG()
-{
-  //Serial.println("Reading ECG");
-  float newData;
-  for (int i = 0; i < 6; i++)
-  {
-    //if ((digitalRead(ECG_PIN) == 1) || (digitalRead(ECG_PIN2) == 1))
-    //{
-      //Serial.println("!");
-    //}
-    //else
-    //{
-    //newData = analogRead(36);
-    newData = random(0, 100) / 100.0;
-    //}
-    doc["ecgdata"]["data"][i] = newData;
-    doc["ecgdata"]["time"][i] = String(micros());
+  // Commented out MAX30105 initialization for now
+  /*
+  Wire.begin(21, 22, 100000L);
+  if (!particleSensor.begin(Wire)) {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
   }
-  return 0.5;
+
+  byte ledBrightness = 60;
+  byte sampleAverage = 4;
+  byte ledMode = 2;
+  byte sampleRate = 100;
+  int pulseWidth = 411;
+  int adcRange = 4096;
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+  */
 }
 
-float o2 = 0;
+int count = 0;
 
-float readO2()
-{
-  //Serial.println("Reading O2");
-  o2 += 0.1;
-  doc["oxygendata"] = String(o2);
-  return o2;
+void loop() {
+  readSensors();
+  sendWsMessage();
+
+  delay(100); // Send data every second
 }
 
-float readheartRate()
-{
-  //Serial.println("Reading Heart Rate");
-  // need to schedule these
-  doc["heartRate"] = String(60);
-  return 60;
+void setupJSON() {
+  JsonObject ecgdata = doc.createNestedObject("ecgdata");
+  ecgDataArray = ecgdata.createNestedArray("data");
+  ecgTimeArray = ecgdata.createNestedArray("time");
+  
+  for (int i = 0; i < 6; i++) {
+    ecgDataArray.add(0);
+    ecgTimeArray.add(0);
+  }
+
+  doc["oxygendata"] = 0;
+  doc["heartRate"] = 0;
+}
+
+void readSensors() {
+  bufferLength = 2;
+  
+  // Simulated sensor readings
+  for (int i = 0; i < 6; i++) {
+    float newECGValue = analogRead(36);
+    ecgDataArray[i] = newECGValue;
+    ecgTimeArray[i] = String(micros());
+  }
+
+  doc["oxygendata"] = random(90, 100);
+  doc["heartRate"] = random(60, 100);
+}
+
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT) {
+    Serial.println("Websocket client connection received");
+  } else if (type == WS_EVT_DISCONNECT) {
+    Serial.println("Client disconnected");
+  } else if (type == WS_EVT_DATA) {
+    Serial.printf("Data received: %s\n", data);
+    client->text("Hello from ESP32 Server");
+    deserializeJson(doc2, data);
+  }
+}
+
+void sendWsMessage() {
+  String json;
+  serializeJson(doc, json);
+  Serial.println(json);
+  ws.textAll(json);
+}
+
+void sendLoraMessage() {
+  String json;
+  serializeJson(doc, json);
+  digitalWrite(LORA, HIGH);
+  Serial.println(json);
+  digitalWrite(LORA, LOW);
 }
