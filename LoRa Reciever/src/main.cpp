e356a1ed00dfcb98a61f175acc5faeb92a27c41d
Ali Ellabban <aliellabban2004@gmail.com>
2024-06-23 17:58:58 +0100

    Added ESP on Reciever side. ESP-32 ip address out.

--- LoRa Reciever/src/main.cpp
+++ LoRa Reciever/src/main.cpp
@@ -1,50 +1,124 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
 
-//define the pins used by the transceiver module
+
+const char *ssid = "iPhone(222)";
+const char *password = "idrees12373";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+JsonDocument doc;
+
+// define the pins used by the transceiver module
 #define ss 0
 #define rst 2
 #define dio0 16
 
-void setup() {
-  //initialize Serial Monitor
-  Serial.begin(9600);
-  while (!Serial);
+void notifyClients(String thing);
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+
+void setup()
+{
+  // initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial)
+    ;
   Serial.println("LoRa Receiver");
 
-  //setup LoRa transceiver module
+  // setup LoRa transceiver module
   LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
-  //433E6 for Asia
-  //866E6 for Europe
-  //915E6 for North America
-  while (!LoRa.begin(433E6)) {
+
+  // replace the LoRa.begin(---E-) argument with your location's frequency
+  // 433E6 for Asia
+  while (!LoRa.begin(433E6))
+  {
     Serial.println(".");
     delay(500);
   }
-   // Change sync word (0xF3) to match the receiver
+  // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
   // ranges from 0-0xFF
   LoRa.setSyncWord(0x44);
   Serial.println("LoRa Initializing OK!");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println(WiFi.localIP());
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+
+  server.begin();
+  Serial.println("Web server started!");
 }
 
-void loop() {
+void loop()
+{
   // try to parse packet
   int packetSize = LoRa.parsePacket();
-  if (packetSize) {
+  if (packetSize)
+  {
     // received a packet
     Serial.print("Received packet '");
 
     // read packet
-    while (LoRa.available()) {
+    while (LoRa.available())
+    {
       String LoRaData = LoRa.readString();
-      Serial.print(LoRaData); 
+      Serial.print(LoRaData);
+      notifyClients(LoRaData);
     }
 
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
   }
+}
+
+void notifyClients(String thing)
+{
+  ws.textAll(thing);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  Serial.printf("Received message: ");
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  switch (type)
+  {
+  case WS_EVT_CONNECT:
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    break;
+  case WS_EVT_DATA:
+    handleWebSocketMessage(arg, data, len);
+    break;
+  case WS_EVT_PONG:
+  case WS_EVT_ERROR:
+    break;
+  }
+}
+
+void initWebSocket()
+{
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
 }
